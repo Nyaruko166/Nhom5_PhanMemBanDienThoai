82417d0bbe21fcbf3db73bf49bea4a59d16bd916
Quanpm_PH27325 <quanpmph27325@fpt.edu.vn>
2022-11-21 14:00:41 +0700

    Init Project

---
+++ src/main/java/com/fpt/it17326/nhom5/repository/AnhRepository.java
@@ -0,0 +1,9 @@
+package com.fpt.it17326.nhom5.repository;
+
+/**
+ * AowVN_Nyaruko
+ **/
+
+public class AnhRepository {
+
+}
---
+++ src/main/java/com/fpt/it17326/nhom5/service/AnhService.java
@@ -0,0 +1,9 @@
+package com.fpt.it17326.nhom5.service;
+
+/**
+ * AowVN_Nyaruko
+ **/
+
+public interface AnhService {
+
+}
