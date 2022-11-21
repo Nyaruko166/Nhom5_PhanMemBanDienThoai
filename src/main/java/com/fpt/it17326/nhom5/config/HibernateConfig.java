aa29f5636e18cc8c566a7d5a96cad3c03dd1905d
Quanpm_PH27325 <quanpmph27325@fpt.edu.vn>
2022-11-21 13:54:48 +0700

    InitProject

---
+++ src/main/java/com/fpt/it17326/nhom5/config/HibernateConfig.java
@@ -0,0 +1,43 @@
+package com.fpt.it17326.nhom5.config;
+
+import java.util.Properties;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateConfig {
+
+    private static final SessionFactory FACTORY;
+
+    static {
+        Configuration conf = new Configuration();
+
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
+        properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=QUANLYBANDIENTHOAI");
+        properties.put(Environment.USER, "nya");
+        properties.put(Environment.PASS, "123456");
+        properties.put(Environment.SHOW_SQL, "true");
+
+        conf.setProperties(properties);
+        //Add @Entity
+        //conf.addAnnotatedClass(*);
+        
+
+        ServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .applySettings(conf.getProperties()).build();
+        FACTORY = conf.buildSessionFactory(registry);
+
+    }
+
+    public static SessionFactory getFACTORY() {
+        return FACTORY;
+    }
+
+//    public static void main(String[] args) {
+//        getFACTORY();
+//    }
+}
