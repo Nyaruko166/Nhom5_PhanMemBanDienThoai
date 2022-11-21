f7899f202f4ff52a44cc17838029a41f19ea6f2b
tungttph27337 <tungttph27337@fpt.edu.vn>
2022-11-21 22:51:12 +0700

    commit

--- src/main/java/com/fpt/it17326/nhom5/view/ViewPin.java
+++ src/main/java/com/fpt/it17326/nhom5/view/ViewPin.java
@@ -32,11 +32,11 @@ public class ViewPin extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 633, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 346, Short.MAX_VALUE)
         );
 
         pack();
