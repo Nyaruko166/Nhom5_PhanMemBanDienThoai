c0c2f420bedcfe7885c2cd605603d256feeba01d
hieundph27275 <hieundph27275@fpt.edu.vn>
2022-11-22 08:38:38 +0700

    check hộ

--- src/main/java/com/fpt/it17326/nhom5/domainmodel/DoiTra.java
+++ src/main/java/com/fpt/it17326/nhom5/domainmodel/DoiTra.java
@@ -1,9 +1,53 @@
 package com.fpt.it17326.nhom5.domainmodel;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * AowVN_Nyaruko
  **/
 
+@Entity
+@Table (name = "DoiTra")
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+
 public class DoiTra {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MaDoiTra")
+    private String MaMaDoiTra;
+    
+    @Column(name="MaHD")
+    private String MaHD;
+    
+    @Column(name="LyDoDoiTra")
+    private String LyDoDoiTra;
+    
+    @Column (name="HanDoiTra")
+    private String HanDoiTra;
+    
+    @Column (name="TrangThai")
+    private boolean TrangThai;
+    
+    @Column (name="CreatedAt")
+    private Date CreatedAt;
+    
+    @Column (name="Deleted")
+    private boolean Deleted;
+    
+    @Column (name="UpdatedAt")
+    private Date UpdaDateAt;
 }
